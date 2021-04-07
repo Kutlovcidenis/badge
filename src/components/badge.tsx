2902a5b2a0c77aea166c92d13cd3224789b01fac
Kutlovcidenis <deniskutlovci@sid1b509620.fritz.box>
2021-04-07 15:08:34 +0200

    added variables for text width and height that changes prior to big or small badge size

--- src/components/badge.tsx
+++ src/components/badge.tsx
@@ -6,17 +6,30 @@ const sizePixel = {
   small: 120,
 };
 
+const bigTextBox ={
+  width: 126.5,
+  height: 96,
+}
+
+const smallTextBox ={
+  width: 86,
+  height: 88,
+}
+
 const bgColor = {
   magenta: '#e20074',
   white: '#ffffff',
   black: '#000000',
 }
 
+
 @Component({
   tag: 'scale-badge',
   styleUrl: 'badge.css',
   shadow: true,
 })
+
+
 export class Badge {
   @Prop() size: 'big' | 'small' = 'big'
   @Prop() color: 'magenta' | 'white' | 'black' | string = "magenta";
@@ -27,9 +40,10 @@ export class Badge {
       --badge-size: ${sizePixel[this.size]}px;
       --badge-color: ${this.color === "magenta" || this.color === "black" || this.color === "white" ? bgColor[this.color] : this.color};
       --badge-rotation: ${this.rotation}deg; 
-  }`;
-}
-
+      --badge-text-width:${this.size === "big" ? bigTextBox.width : smallTextBox.width}px;
+      --badge-text-height:${this.size === "big" ? bigTextBox.height : smallTextBox.height}px;
+    }`;
+  }
 
 
   render() {
