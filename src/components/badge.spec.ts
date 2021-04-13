65faacd7f0c384cea01f8d95016e03ccc5cc7387
Kutlovcidenis <deniskutlovci@sid1b509620.fritz.box>
2021-04-13 12:04:01 +0200

    New tests, coverage 100%, varibale styling in index.html

--- src/components/badge.spec.ts
+++ src/components/badge.spec.ts
@@ -2,7 +2,7 @@ import { newSpecPage } from '@stencil/core/testing';
 import { Badge } from './badge';
 
 
-it ("should reflect attributes", async () => {
+it ("should reflect attributes/props", async () => {
     let page = await newSpecPage({
         components: [Badge],
         html:`<scale-badge
@@ -17,6 +17,7 @@ it ("should reflect attributes", async () => {
     expect(page.rootInstance.rotation).toBe(15);
 })
 
+
 it ("Should add 10 to the rotation", async () => {
     const badge = new Badge(); 
     badge.rotation = 10;
@@ -25,17 +26,39 @@ it ("Should add 10 to the rotation", async () => {
 });
 
 
-it ("correct texbox size", async () => {
+it ('check style --badge-text-width is ${width} for size="big"', async () => {
+    const widthBig = '126.5px';
+    let page = await newSpecPage({
+        components: [Badge],
+        html:`<scale-badge size="big"></scale-badge>`
+    });
+    const styles = page.rootInstance.displayStyle();
+    expect(styles.includes(`--badge-text-width:${widthBig}`)).toBe(true);
+})
+
+
+it ('check style --badge-text-width is ${width} for size="small"', async () => {
+    const widthSmall = '86px';
+    let page = await newSpecPage({
+        components: [Badge],
+        html:`<scale-badge size="small"></scale-badge>`
+    });
+    const styles = page.rootInstance.displayStyle();
+    expect(styles.includes(`--badge-text-width:${widthSmall}`)).toBe(true);
+})
+
+
+it ("checks another color, other than prop", async () => {
     let page = await newSpecPage({
         components: [Badge],
         html:`<scale-badge
                 size ="big"
-                color ="magenta"
+                color ="red"
                 rotation ="15">
               </scale-badge>`
     });
 
-    expect(page.rootInstance.className).toBe("big");
-    expect(page.rootInstance.color).toBe("magenta");
+    expect(page.rootInstance.size).toBe("big");
+    expect(page.rootInstance.color).toBe("red");
     expect(page.rootInstance.rotation).toBe(15);
 })
